c157f0960ea41e72b282d68af561b1143fad389c
basesnel <basesnel@gmail.com>
2024-03-30 21:26:42 +0200

    Added some positions for arrowRight and arrowLeft keys behaviors on phoneInput.

--- js/phone-input.js
+++ js/phone-input.js
@@ -7,7 +7,7 @@ phoneEl.addEventListener('blur', handlePhoneBlur);
 
 const maskType = '+38 (0__) ___-__-__';
 const initPosition = 6;
-const caretPositions = [6, 7, 10, 11, 12, 14, 15, 17, 18, 19];
+const caretPositions = [6, 7, 8, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
 
 function handlePhoneFocus() {
   setTimeout(() => {
