a917001be81d86578a12a89147e237a2f953a5d6
basesnel <basesnel@gmail.com>
2024-04-04 13:34:02 +0300

    Changed function editPhoneNumber.

--- js/phone-input.js
+++ js/phone-input.js
@@ -101,30 +101,16 @@ function editPhoneNumber(pressedKey, caretPosition, backspace = false) {
         changePhoneValue(phoneEl, pressedKey);
         setCaretPosition(phoneEl, caretPosition + 1);
       } else {
-        switch (caretPosition) {
-          case 8:
-            setCaretPosition(phoneEl, 10);
-            changePhoneValue(phoneEl, pressedKey);
-            setCaretPosition(phoneEl, 11);
-            break;
-
-          case 13:
-            setCaretPosition(phoneEl, 14);
-            changePhoneValue(phoneEl, pressedKey);
-            setCaretPosition(phoneEl, 15);
-            break;
-
-          case 16:
-            setCaretPosition(phoneEl, 17);
-            changePhoneValue(phoneEl, pressedKey);
-            setCaretPosition(phoneEl, 18);
-            break;
-
-          case 19:
-            caretPosition = setCaretPosition(phoneEl, 6);
-            changePhoneValue(phoneEl, pressedKey);
-            setCaretPosition(phoneEl, 7);
-            break;
+        const iom = rightMargins.indexOf(caretPosition);
+        if (iom !== rightMargins.length - 1) {
+          const iop = caretPositions.indexOf(caretPosition);
+          setCaretPosition(phoneEl, caretPositions[iop + 1]);
+          changePhoneValue(phoneEl, pressedKey);
+          setCaretPosition(phoneEl, caretPositions[iop + 2]);
+        } else {
+          setCaretPosition(phoneEl, caretPositions[0]);
+          changePhoneValue(phoneEl, pressedKey);
+          setCaretPosition(phoneEl, caretPositions[1]);
         }
       }
     } else {
@@ -133,42 +119,29 @@ function editPhoneNumber(pressedKey, caretPosition, backspace = false) {
       setCaretPosition(phoneEl, caretPositions[1]);
     }
   } else {
+    const length = caretPositions.length;
     if (caretPositions.includes(caretPosition)) {
       if (!leftMargins.includes(caretPosition)) {
         setCaretPosition(phoneEl, caretPosition - 1);
         changePhoneValue(phoneEl, maskChar);
         setCaretPosition(phoneEl, caretPosition - 1);
       } else {
-        switch (caretPosition) {
-          case 10:
-            setCaretPosition(phoneEl, 7);
-            changePhoneValue(phoneEl, maskChar);
-            setCaretPosition(phoneEl, 7);
-            break;
-
-          case 14:
-            setCaretPosition(phoneEl, 12);
-            changePhoneValue(phoneEl, maskChar);
-            setCaretPosition(phoneEl, 12);
-            break;
-
-          case 17:
-            setCaretPosition(phoneEl, 15);
-            changePhoneValue(phoneEl, maskChar);
-            setCaretPosition(phoneEl, 15);
-            break;
-
-          case 6:
-            caretPosition = setCaretPosition(phoneEl, 18);
-            changePhoneValue(phoneEl, maskChar);
-            setCaretPosition(phoneEl, 18);
-            break;
+        const iom = leftMargins.indexOf(caretPosition);
+        if (iom !== leftMargins.length - 1) {
+          const iop = caretPositions.indexOf(caretPosition);
+          setCaretPosition(phoneEl, caretPositions[iop - 2]);
+          changePhoneValue(phoneEl, pressedKey);
+          setCaretPosition(phoneEl, caretPositions[iop - 2]);
+        } else {
+          setCaretPosition(phoneEl, caretPositions[length - 2]);
+          changePhoneValue(phoneEl, pressedKey);
+          setCaretPosition(phoneEl, caretPositions[length - 2]);
         }
       }
     } else {
-      setCaretPosition(phoneEl, caretPositions[18]);
+      setCaretPosition(phoneEl, caretPositions[length - 2]);
       changePhoneValue(phoneEl, maskChar);
-      setCaretPosition(phoneEl, caretPositions[18]);
+      setCaretPosition(phoneEl, caretPositions[length - 2]);
     }
   }
 }
