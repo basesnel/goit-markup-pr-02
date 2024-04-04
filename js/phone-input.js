6acdf14176420e3fe3f8767210c3ac757515474c
basesnel <basesnel@gmail.com>
2024-04-04 12:34:03 +0300

    Added function editPhoneNumber.

--- js/phone-input.js
+++ js/phone-input.js
@@ -8,6 +8,8 @@ phoneEl.addEventListener('blur', handlePhoneBlur);
 const maskType = '+38 (0__) ___-__-__';
 const maskChar = '_';
 const caretPositions = [6, 7, 8, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
+const rightMargins = [8, 13, 16, 19];
+const leftMargins = [10, 14, 17, 6];
 
 function handlePhoneFocus() {
   setTimeout(() => {
@@ -15,7 +17,7 @@ function handlePhoneFocus() {
       phoneEl.value = maskType;
       setCaretPosition(phoneEl, caretPositions[0]);
     }
-  }, 250);
+  }, 100);
 }
 
 function handlePhoneBlur() {
@@ -23,7 +25,7 @@ function handlePhoneBlur() {
     if (phoneEl.value === maskType) {
       phoneEl.value = '';
     }
-  }, 250);
+  }, 100);
 }
 
 function setPhoneMask(e) {
@@ -35,126 +37,17 @@ function setPhoneMask(e) {
 
   if (pressedKey === 'Delete') {
     let caretPosition = phoneEl.selectionStart;
-
-    if (caretPositions.includes(caretPosition)) {
-      if (![8, 13, 16, 19].includes(caretPosition)) {
-        changePhoneValue(phoneEl, maskChar);
-        setCaretPosition(phoneEl, caretPosition + 1);
-      } else {
-        switch (caretPosition) {
-          case 8:
-            setCaretPosition(phoneEl, 10);
-            changePhoneValue(phoneEl, maskChar);
-            setCaretPosition(phoneEl, 11);
-            break;
-
-          case 13:
-            setCaretPosition(phoneEl, 14);
-            changePhoneValue(phoneEl, maskChar);
-            setCaretPosition(phoneEl, 15);
-            break;
-
-          case 16:
-            setCaretPosition(phoneEl, 17);
-            changePhoneValue(phoneEl, maskChar);
-            setCaretPosition(phoneEl, 18);
-            break;
-
-          case 19:
-            caretPosition = setCaretPosition(phoneEl, 6);
-            changePhoneValue(phoneEl, maskChar);
-            setCaretPosition(phoneEl, 7);
-            break;
-        }
-      }
-    } else {
-      setCaretPosition(phoneEl, caretPositions[0]);
-      changePhoneValue(phoneEl, maskChar);
-      setCaretPosition(phoneEl, caretPositions[1]);
-    }
+    editPhoneNumber(maskChar, caretPosition);
   }
 
   if (pressedKey === 'Backspace') {
     let caretPosition = phoneEl.selectionStart;
-
-    if (caretPositions.includes(caretPosition)) {
-      if (![10, 14, 17, 6].includes(caretPosition)) {
-        setCaretPosition(phoneEl, caretPosition - 1);
-        changePhoneValue(phoneEl, maskChar);
-        setCaretPosition(phoneEl, caretPosition - 1);
-      } else {
-        switch (caretPosition) {
-          case 10:
-            setCaretPosition(phoneEl, 7);
-            changePhoneValue(phoneEl, maskChar);
-            setCaretPosition(phoneEl, 7);
-            break;
-
-          case 14:
-            setCaretPosition(phoneEl, 12);
-            changePhoneValue(phoneEl, maskChar);
-            setCaretPosition(phoneEl, 12);
-            break;
-
-          case 17:
-            setCaretPosition(phoneEl, 15);
-            changePhoneValue(phoneEl, maskChar);
-            setCaretPosition(phoneEl, 15);
-            break;
-
-          case 6:
-            caretPosition = setCaretPosition(phoneEl, 18);
-            changePhoneValue(phoneEl, maskChar);
-            setCaretPosition(phoneEl, 18);
-            break;
-        }
-      }
-    } else {
-      setCaretPosition(phoneEl, caretPositions[18]);
-      changePhoneValue(phoneEl, maskChar);
-      setCaretPosition(phoneEl, caretPositions[18]);
-    }
+    editPhoneNumber(maskChar, caretPosition, true);
   }
 
   if (/^([0-9])$/.test(pressedKey)) {
     let caretPosition = phoneEl.selectionStart;
-
-    if (caretPositions.includes(caretPosition)) {
-      if (![8, 13, 16, 19].includes(caretPosition)) {
-        changePhoneValue(phoneEl, pressedKey);
-        setCaretPosition(phoneEl, caretPosition + 1);
-      } else {
-        switch (caretPosition) {
-          case 8:
-            setCaretPosition(phoneEl, 10);
-            changePhoneValue(phoneEl, pressedKey);
-            setCaretPosition(phoneEl, 11);
-            break;
-
-          case 13:
-            setCaretPosition(phoneEl, 14);
-            changePhoneValue(phoneEl, pressedKey);
-            setCaretPosition(phoneEl, 15);
-            break;
-
-          case 16:
-            setCaretPosition(phoneEl, 17);
-            changePhoneValue(phoneEl, pressedKey);
-            setCaretPosition(phoneEl, 18);
-            break;
-
-          case 19:
-            caretPosition = setCaretPosition(phoneEl, 6);
-            changePhoneValue(phoneEl, pressedKey);
-            setCaretPosition(phoneEl, 7);
-            break;
-        }
-      }
-    } else {
-      setCaretPosition(phoneEl, caretPositions[0]);
-      changePhoneValue(phoneEl, pressedKey);
-      setCaretPosition(phoneEl, caretPositions[1]);
-    }
+    editPhoneNumber(pressedKey, caretPosition);
   }
 
   if (pressedKey === 'ArrowRight') {
@@ -199,6 +92,85 @@ function changePhoneValue(el, key) {
   const changedValue = el.value.split('');
   changedValue[el.selectionStart] = key;
   el.value = changedValue.join('');
+}
+
+function editPhoneNumber(pressedKey, caretPosition, backspace = false) {
+  if (!backspace) {
+    if (caretPositions.includes(caretPosition)) {
+      if (!rightMargins.includes(caretPosition)) {
+        changePhoneValue(phoneEl, pressedKey);
+        setCaretPosition(phoneEl, caretPosition + 1);
+      } else {
+        switch (caretPosition) {
+          case 8:
+            setCaretPosition(phoneEl, 10);
+            changePhoneValue(phoneEl, pressedKey);
+            setCaretPosition(phoneEl, 11);
+            break;
+
+          case 13:
+            setCaretPosition(phoneEl, 14);
+            changePhoneValue(phoneEl, pressedKey);
+            setCaretPosition(phoneEl, 15);
+            break;
+
+          case 16:
+            setCaretPosition(phoneEl, 17);
+            changePhoneValue(phoneEl, pressedKey);
+            setCaretPosition(phoneEl, 18);
+            break;
+
+          case 19:
+            caretPosition = setCaretPosition(phoneEl, 6);
+            changePhoneValue(phoneEl, pressedKey);
+            setCaretPosition(phoneEl, 7);
+            break;
+        }
+      }
+    } else {
+      setCaretPosition(phoneEl, caretPositions[0]);
+      changePhoneValue(phoneEl, pressedKey);
+      setCaretPosition(phoneEl, caretPositions[1]);
+    }
+  } else {
+    if (caretPositions.includes(caretPosition)) {
+      if (!leftMargins.includes(caretPosition)) {
+        setCaretPosition(phoneEl, caretPosition - 1);
+        changePhoneValue(phoneEl, maskChar);
+        setCaretPosition(phoneEl, caretPosition - 1);
+      } else {
+        switch (caretPosition) {
+          case 10:
+            setCaretPosition(phoneEl, 7);
+            changePhoneValue(phoneEl, maskChar);
+            setCaretPosition(phoneEl, 7);
+            break;
+
+          case 14:
+            setCaretPosition(phoneEl, 12);
+            changePhoneValue(phoneEl, maskChar);
+            setCaretPosition(phoneEl, 12);
+            break;
+
+          case 17:
+            setCaretPosition(phoneEl, 15);
+            changePhoneValue(phoneEl, maskChar);
+            setCaretPosition(phoneEl, 15);
+            break;
+
+          case 6:
+            caretPosition = setCaretPosition(phoneEl, 18);
+            changePhoneValue(phoneEl, maskChar);
+            setCaretPosition(phoneEl, 18);
+            break;
+        }
+      }
+    } else {
+      setCaretPosition(phoneEl, caretPositions[18]);
+      changePhoneValue(phoneEl, maskChar);
+      setCaretPosition(phoneEl, caretPositions[18]);
+    }
+  }
 }
 
 // //Функция маски инпута
