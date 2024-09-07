fda50cd1ccebcc80866794adb237cc86a1262791
basesnel <basesnel@gmail.com>
2024-09-07 19:33:10 +0300

    Add slider script

---
+++ js/slider.js
@@ -0,0 +1,56 @@
+const sliderLine = document.querySelector('.slider__line');
+const prevButton = document.querySelector('.slider__prev');
+const nextButton = document.querySelector('.slider__next');
+const dots = document.querySelectorAll('.slider__dot');
+
+let position = 0;
+let dotIndex = 0;
+
+const nextSlide = () => {
+  if (position < (dots.length - 1) * 346) {
+    position += 346;
+    dotIndex++;
+  } else {
+    position = 0;
+    dotIndex = 0;
+  }
+
+  sliderLine.style.left = -position + 'px';
+  thisSlide(dotIndex);
+};
+
+const prevSlide = () => {
+  if (position > 0) {
+    position -= 346;
+    dotIndex--;
+  } else {
+    position = (dots.length - 1) * 346;
+    dotIndex = dots.length - 1;
+  }
+
+  sliderLine.style.left = -position + 'px';
+  thisSlide(dotIndex);
+};
+
+const thisSlide = index => {
+  for (let dot of dots) {
+    dot.classList.remove('slider__dot--active');
+  }
+  dots[index].classList.add('slider__dot--active');
+};
+
+nextButton.addEventListener('click', nextSlide);
+prevButton.addEventListener('click', prevSlide);
+
+dots.forEach((dot, index) => {
+  dot.addEventListener('click', () => {
+    position = 346 * index;
+    sliderLine.style.left = -position + 'px';
+    dotIndex = index;
+    thisSlide(dotIndex);
+  });
+});
+
+setInterval(() => {
+  nextSlide();
+}, 10000);
