f14f1ee18638856023c878bbeef17b5fa9a328e5
basesnel <basesnel@gmail.com>
2024-09-09 15:00:55 +0300

    Applies actual photos in the slider.

--- js/slider.js
+++ js/slider.js
@@ -2,34 +2,35 @@ const sliderLine = document.querySelector('.slider__line');
 const prevButton = document.querySelector('.slider__button--prev');
 const nextButton = document.querySelector('.slider__button--next');
 const dots = document.querySelectorAll('.slider__dot');
+const itemWidth = sliderLine.offsetWidth;
+console.log(itemWidth);
+console.dir(sliderLine);
 
 let position = 0;
 let dotIndex = 0;
 
 const nextSlide = () => {
-  if (position < (dots.length - 1) * 346) {
-    position += 346;
+  if (position < (dots.length - 1) * itemWidth) {
+    position += itemWidth;
     dotIndex++;
   } else {
     position = 0;
     dotIndex = 0;
   }
 
-  // sliderLine.style.left = -position + 'px';
   sliderLine.style.transform = `translateX(${-position}px)`;
   thisSlide(dotIndex);
 };
 
 const prevSlide = () => {
   if (position > 0) {
-    position -= 346;
+    position -= itemWidth;
     dotIndex--;
   } else {
-    position = (dots.length - 1) * 346;
+    position = (dots.length - 1) * itemWidth;
     dotIndex = dots.length - 1;
   }
 
-  // sliderLine.style.left = -position + 'px';
   sliderLine.style.transform = `translateX(${-position}px)`;
   thisSlide(dotIndex);
 };
@@ -46,7 +47,7 @@ prevButton.addEventListener('click', prevSlide);
 
 // dots.forEach((dot, index) => {
 //   dot.addEventListener('click', () => {
-//     position = 346 * index;
+//     position = itemWidth * index;
 //     sliderLine.style.left = -position + 'px';
 //     dotIndex = index;
 //     thisSlide(dotIndex);
@@ -55,8 +56,7 @@ prevButton.addEventListener('click', prevSlide);
 
 for (const [index, dot] of dots.entries()) {
   dot.addEventListener('click', () => {
-    position = 346 * index;
-    // sliderLine.style.left = -position + 'px';
+    position = itemWidth * index;
     sliderLine.style.transform = `translateX(${-position}px)`;
     dotIndex = index;
     thisSlide(dotIndex);
